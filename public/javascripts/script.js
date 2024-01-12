6d57805d59004c411fa0655b013b46da8c6129e5
Abhishek Ahirwar <abhishek738950@gmail.com>
2024-01-12 12:04:12 +0530

    Pinterest Clone

---
+++ public/javascripts/script.js
@@ -0,0 +1,49 @@
+gsap.from(" .first-box ",{
+    y:400,
+    opecity:0,
+    duration:2,
+    delay:0.5,
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true,
+})
+
+gsap.from(" .second-box ",{
+    y:300,
+    opecity:0,
+    duration:2,
+    delay:0.7,
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true,
+})
+
+gsap.from(" .third-box ",{
+    y:200,
+    opecity:0,
+    duration:2,
+    delay:0.9,
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true,
+})
+
+gsap.from(" .center-box ",{
+    y:100,
+    opecity:0,
+    duration:2,
+    delay:1.1,
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true,
+})
+
+
+// gsap.from("#image", {
+//     y:600,
+//     opecity:0,
+//     duration:3,
+//     delay:0.5,
+//     stagger:.5,
+//     repeat:-1,
+// })
